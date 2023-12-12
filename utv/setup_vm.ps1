e5ce384258f3b5587760bf5b76d5e3f38ad0f652
egraarud <141819281+egraarud@users.noreply.github.com>
2023-12-12 18:04:39 +0100

    Create setup_vm.ps1

---
+++ utv/setup_vm.ps1
@@ -0,0 +1,24 @@
+#Disable ieESC
+function Disable-ieESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+Disable-ieESC
+
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Install Software
+choco install sql-server-management-studio -y
+choco install visualstudiocode -y
+choco install python -y
+choco install git -y
+choco install googlechrome -y
+choco install powerbi -y
+choco install daxstudio -y
+choco install visualstudio2019enterprise -y
+choco install ssis-vs2019 -y
